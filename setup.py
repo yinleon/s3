6964232c8d7277eda43dc1e814f2c849abfa49c0
Leon Yin <leonyin@172-16-29-73.DYNAPOOL.NYU.EDU>
2018-03-07 16:08:42 -0500

    added scipy requirement

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='s34me',
 	packages=['s3'],
-	version='0.0.8',
+	version='0.0.9',
 	description='s34me is a high-level wrapper for handling s3 objects in boto3 and Pandas',
 	author='leon yin',
 	author_email='ly501@nyu.edu',
@@ -14,6 +14,7 @@ setup(name='s34me',
 		'pandas<=0.19.2',
 		'boto3>=1.4.4',
 		'botocore>=1.5.7',
-		'scikit-learn>=0.18.1'
+		'scikit-learn>=0.18.1',
+		'scipy>=1.0.0'
 	]
 )
